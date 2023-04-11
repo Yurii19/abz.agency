422f2be5781a4c495967865fdac591338fbb41f6
Yurii19 <yurikolechkin@gmail.com>
2023-04-11 08:53:15 +0300

    style: added sass loader

--- src/layouts/LoginPage.vue
+++ src/layouts/LoginPage.vue
@@ -1,19 +1,18 @@
 <template>
-    <div class="container">
-        <span>Hello world</span>
-
-    </div>
+  <div class="container">
+    <span>Hello world</span>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref, watch } from 'vue';
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .container {
-    color: white;
-}
-span {
-    font-size: 2rem;
+  color: white;
+  span {
+    font-size: 40px;
+  }
 }
 </style>
